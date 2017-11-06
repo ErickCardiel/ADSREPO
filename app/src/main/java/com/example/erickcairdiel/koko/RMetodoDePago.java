8c0e7b7cca65bd998fc653203ace9aa5187db6c1
Erick Cairdiel <gato_ryuzaki@hotmail.com>
2017-11-06 13:53:40 -0800

    Arregle enlaces entre las pantallas y warnings que tenia en la v anterior.

--- app/src/main/java/com/example/erickcairdiel/koko/RMetodoDePago.java
+++ app/src/main/java/com/example/erickcairdiel/koko/RMetodoDePago.java
@@ -8,20 +8,30 @@ import android.widget.Button;
 
 public class RMetodoDePago extends AppCompatActivity {
 
-    Button siguiente;
+    Button tarjeta,siguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rmetodo_de_pago);
 
-        siguiente = (Button)findViewById(R.id.btnSiguiente);
-        siguiente.setOnClickListener(new View.OnClickListener() {
+        tarjeta = (Button)findViewById(R.id.btnTarjeta);
+        tarjeta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent siguientePantalla = new Intent(RMetodoDePago.this,RDatosTarjeta.class);
                 startActivity(siguientePantalla);
             }
         });
+
+        siguiente = (Button)findViewById(R.id.btnSiguiente);
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent siguientePantalla2 = new Intent(RMetodoDePago.this,RTipoDeCuenta.class);
+                startActivity(siguientePantalla2);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/erickcairdiel/koko/RTipoDeCuenta.java
+++ app/src/main/java/com/example/erickcairdiel/koko/RTipoDeCuenta.java
@@ -8,20 +8,32 @@ import android.widget.Button;
 
 public class RTipoDeCuenta extends AppCompatActivity {
 
-    Button conductor;
+    Button conductor,estudiante;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rtipo_de_cuenta);
 
-        conductor = (Button)findViewById(R.id.btnSiguiente);
+        conductor = (Button)findViewById(R.id.btnConductor);
+        estudiante = (Button)findViewById(R.id.btnEstudiante);
+
+
         conductor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
                 Intent siguientePantalla = new Intent(RTipoDeCuenta.this,RConductorDatosAutomovil.class);
                 startActivity(siguientePantalla);
+            }
+        });
+
+        estudiante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent siguientePantalla2 = new Intent(RTipoDeCuenta.this,PrincipalEstudiante.class);
+                startActivity(siguientePantalla2);
             }
         });
     }
