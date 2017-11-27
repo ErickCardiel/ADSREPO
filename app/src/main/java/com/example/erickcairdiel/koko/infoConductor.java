f6ce300e0ff1280613a3fd7d401d382a8b00de93
Bianca Sanchez <yereline.sanchez@uabc.edu.mx>
2017-11-26 18:30:03 -0800

    Se agregaron Activitis faltantes

---
+++ app/src/main/java/com/example/erickcairdiel/koko/infoConductor.java
@@ -0,0 +1,35 @@
+package com.example.erickcairdiel.koko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class infoConductor extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    RatingBar ratingBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_conductor);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ratingBar = (RatingBar) findViewById(R.id.ICalificacion);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                Toast.makeText(infoConductor.this,"Start: "+ v,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+}
